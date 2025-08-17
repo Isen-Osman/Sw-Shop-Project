7b8ccb43a99d4802d5f275dbe175239b40e6bbde
isendev <isen.osman252@gmail.com>
2025-08-17 14:09:52 +0200

    Save local changes before merge

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,26 +1,17 @@
-"""
-URL configuration for DjangoProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/add/', views.product_add, name='product_add'),
+    path('product/<int:product_id>/delete/', views.product_delete, name='product_delete'),
+    path('product/<int:product_id>/edit/', views.product_edit, name='product_edit'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+
 ]
 
 if settings.DEBUG:
--- app/views.py
+++ app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ProductForm
+from .models import Product
 
-# Create your views here.
+
+def product_add(request):
+    if request.method == 'POST':
+        product_form = ProductForm(request.POST)
+        if product_form.is_valid():
+            product = product_form.save()
+            return redirect('product_list')
+    else:
+        product_form = ProductForm()
+
+    return render(request, 'products/product_add.html', {'form': product_form})
+
+
+def product_edit(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'products/product_edit.html', {'form': form, 'product': product})
+
+def product_delete(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+
+    return render(request, 'products/product_delete.html', {'product': product})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
