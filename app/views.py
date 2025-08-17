d11457ac551f8570647d12f2bd8aa13d131d095d
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-17 18:08:51 +0200

    Update v3

--- app/views.py
+++ app/views.py
@@ -6,11 +6,20 @@ from .models import Product, ProductImage, Category
 
 def product_list(request):
     products = Product.objects.all()
+
+    # Додај нов атрибут за новата цена
+    for product in products:
+        product.new_price = product.price + 200  # новата променлива
+
     return render(request, 'product_page.html', {'products': products})
 
 
 def recently_added_products(request):
     products = Product.objects.all().order_by('-created_at')[:5]  # последни 5 продукти
+
+    # Додај нов атрибут за новата цена
+    for product in products:
+        product.new_price = product.price + 200  # новата променлива
 
     return render(request, 'home_page.html', {'products': products})
 
@@ -18,11 +27,14 @@ def recently_added_products(request):
 def product_add(request):
     if request.method == 'POST':
         product_form = ProductForm(request.POST)
-        images = request.FILES.getlist('images')  # земи сите прикачени фајлови
+
+        # take uploaded images
+        images = request.FILES.getlist('images')
 
         if product_form.is_valid():
             product = product_form.save()
 
+            # save images separately
             for img in images:
                 ProductImage.objects.create(product=product, image=img)
 
@@ -31,6 +43,7 @@ def product_add(request):
         product_form = ProductForm()
 
     return render(request, 'product_add.html', {'form': product_form})
+
 
 
 def product_edit(request, product_id):
@@ -61,7 +74,13 @@ def product_delete(request, product_id):
 
 
 def product_detail(request, product_id):
+    products = Product.objects.all()
     product = get_object_or_404(Product, id=product_id)
+
+    # Додај нов атрибут за новата цена
+    for product in products:
+        product.new_price = product.price + 200  # новата променлива
+
     return render(request, 'product_detail.html', {'product': product})
 
 def products_by_category(request, category_name):
