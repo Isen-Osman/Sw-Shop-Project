fa19cc5cdb02f3ac83b12742bb4f8baa38624ca3
isendev <isen.osman252@gmail.com>
2025-08-17 14:09:52 +0200

    Save local changes before merge

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Product, ProductQuantity, ProductImage
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'color', 'category']
+
+class ProductQuantityForm(forms.ModelForm):
+    class Meta:
+        model = ProductQuantity
+        fields = ['size', 'quantity']
+
+class ProductImageForm(forms.ModelForm):
+    class Meta:
+        model = ProductImage
+        fields = ['image']
