1bdbd6d12ba8070182419d5b670ee79b19bd5e65
isendev <isen.osman252@gmail.com>
2025-08-17 14:28:17 +0200

    Crud and views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import ProductForm
-from .models import Product
+from .models import Product, ProductImage
 
 
 def product_list(request):
@@ -11,36 +11,43 @@ def product_list(request):
 def product_add(request):
     if request.method == 'POST':
         product_form = ProductForm(request.POST)
+        images = request.FILES.getlist('images')  # земи сите прикачени фајлови
+
         if product_form.is_valid():
             product = product_form.save()
-            return redirect('product_list')
+
+            for img in images:
+                ProductImage.objects.create(product=product, image=img)
+
+            return redirect('products')
     else:
         product_form = ProductForm()
 
-    return render(request, 'products/product_add.html', {'form': product_form})
-
-
+    return render(request, 'product_add.html', {'form': product_form})
 def product_edit(request, product_id):
     product = get_object_or_404(Product, id=product_id)
+
     if request.method == 'POST':
         form = ProductForm(request.POST, instance=product)
+        images = request.FILES.getlist('images')
+
         if form.is_valid():
             form.save()
-            return redirect('product_list')
+
+            # Додај нови слики ако има
+            for img in images:
+                ProductImage.objects.create(product=product, image=img)
+
+            return redirect('products')
     else:
         form = ProductForm(instance=product)
 
-    return render(request, 'products/product_edit.html', {'form': form, 'product': product})
-
+    return render(request, 'product_edit.html', {'form': form, 'product': product})
 def product_delete(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    if request.method == 'POST':
-        product.delete()
-        return redirect('product_list')
-
-    return render(request, 'products/product_delete.html', {'product': product})
-
+    product.delete()
+    return redirect('products')
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    return render(request, 'products/product_detail.html', {'product': product})
+    return render(request, 'product_detail.html', {'product': product})
