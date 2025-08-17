bb20726011699b009858532af8559282dbd52cd3
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-17 14:07:59 +0200

    Added a products page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+
+    return render(request, 'product_page.html', {'products': products})
+
+
+# def home_page(request):
+#     products = Product.objects.all().order_by('-created_at')[:5]
+#
+#     return render(request, 'home_page.html', {'products': products})
