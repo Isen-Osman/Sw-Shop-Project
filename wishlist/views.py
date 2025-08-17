484be256ef1a34eca98daf9ab634c552aed69ba1
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-17 18:52:39 +0200

    Update v4

--- wishlist/views.py
+++ wishlist/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def wishlist_view(request):
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+
+    for product in wishlist.products.all():
+        product.new_price = product.price + 200
+
     total_price = sum([product.price for product in wishlist.products.all()])
     return render(request, 'wishlist.html', {'wishlist': wishlist, 'total_price': total_price})
 
