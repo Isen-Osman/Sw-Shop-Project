e07a2919cfe8ffc835912edb3e407e2df2cd5d24
isendev <isen.osman252@gmail.com>
2025-08-20 14:30:58 +0200

    update

---
+++ DjangoProject/settings.py
@@ -0,0 +1,163 @@
+from pathlib import Path
+from decouple import config
+
+# -------------------------------
+# Base directory
+# -------------------------------
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# -------------------------------
+# Security
+# -------------------------------
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# -------------------------------
+# Installed Apps
+# -------------------------------
+INSTALLED_APPS = [
+    # Django default apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+
+    # My apps
+    'app',
+    'wishlist',
+    'orders',
+    'widget_tweaks',
+    'sslserver',
+
+    # Allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # Social providers
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+]
+
+# -------------------------------
+# Authentication backends
+# -------------------------------
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # default
+    'allauth.account.auth_backends.AuthenticationBackend',  # allauth
+]
+
+# -------------------------------
+# Middleware
+# -------------------------------
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+
+    # Allauth middleware
+    'allauth.account.middleware.AccountMiddleware',
+
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# -------------------------------
+# URLs
+# -------------------------------
+ROOT_URLCONF = 'DjangoProject.urls'
+
+# -------------------------------
+# Templates
+# -------------------------------
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# -------------------------------
+# WSGI
+# -------------------------------
+WSGI_APPLICATION = 'DjangoProject.wsgi.application'
+
+# -------------------------------
+# Database
+# -------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# -------------------------------
+# Password validators
+# -------------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# -------------------------------
+# Internationalization
+# -------------------------------
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# -------------------------------
+# Static files
+# -------------------------------
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# -------------------------------
+# Sites framework
+# -------------------------------
+SITE_ID = 1
+
+# -------------------------------
+# Allauth settings
+# -------------------------------
+LOGIN_REDIRECT_URL = '/profile/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_LOGIN_METHODS = {'username', 'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
+
+# -------------------------------
+# Google OAuth
+# -------------------------------
+SOCIAL_AUTH_GOOGLE_CLIENT_ID = config('CLIENT_ID')
+SOCIAL_AUTH_GOOGLE_SECRET = config('SECRET')
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'APP': {
+            'client_id': SOCIAL_AUTH_GOOGLE_CLIENT_ID,
+            'secret': SOCIAL_AUTH_GOOGLE_SECRET,
+            'key': ""
+        }
+    }
+}
